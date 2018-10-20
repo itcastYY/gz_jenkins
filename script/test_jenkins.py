c5d84317eae5f6e45ff80b7cebee3b64941ff5bb
yyshi <yyshi@qq.com>
2018-10-20 22:05:32 +0800

    提交本地的工作区的内容

---
+++ script/test_jenkins.py
@@ -0,0 +1,17 @@
+# -*- coding=utf-8 -*-
+
+
+class TestDemo:
+
+    def test_fn1(self):
+        print( "第一个函数" )
+
+    def test_fn2(self):
+        print( "第二个函数" )
+
+    def test_fn3(self):
+        print( "第三个函数" )
+
+    def test_fn4(self):
+        print( "第四个函数" )
+        assert 0
